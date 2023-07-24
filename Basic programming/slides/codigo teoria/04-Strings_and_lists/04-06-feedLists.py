38a6c4241330059e72dd9d1c4c74605a48ebc858
dcabestany <62064856+dcabesma@users.noreply.github.com>
2023-07-24 22:52:54 +0200

    Delete Basic programming/slides directory

--- Basic programming/slides/codigo teoria/04-Strings_and_lists/04-06-feedLists.py
+++
@@ -1,7 +0,0 @@
-writers = []
-writer = input("Tell me one of your favourite writers")
-while writer != "":
-	writers.append(writer)
-	writer = input("Tell me one of your favourite writers")
-writers.sort()
-print(writers)
